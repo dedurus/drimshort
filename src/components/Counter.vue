40b67df0a8da27986c13eba8102df4d3494e21cc
dedurus <dedurac@gmail.com>
2020-11-11 16:11:18 +0100

    counter main page

---
+++ src/components/Counter.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="counter">
+        {{timerCount}}
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Counter',
+     data() {
+            return {
+                timerCount: ''
+            }
+        },
+
+        watch: {
+
+            timerCount: {
+                handler(value) {
+                    var cd_date = new Date("Nov 12, 2020 23:23:59").getTime();
+
+                    setTimeout(() => {
+                        var now = new Date().getTime();
+                        var diff = cd_date - now;
+                        var d = Math.floor(diff / (1000 * 60 * 60 * 24));
+                        var h = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                        var m = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+                        var s = Math.floor((diff % (1000 * 60)) / 1000);
+                        return this.timerCount = d + "d " + h + "h " + m + "m " + s + "s ";
+                    }, 1000);
+
+                },
+                immediate: true // This ensures the watcher is triggered upon creation
+            }
+
+        }
+}
+</script>
