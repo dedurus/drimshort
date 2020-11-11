913ca9d3321730d3295c76b18b5737cb97185c6a
dedurus <dedurac@gmail.com>
2020-11-11 16:10:32 +0100

    new program

--- src/pages/Program2020.vue
+++ src/pages/Program2020.vue
@@ -5,135 +5,223 @@
 
     <div id="program_tabs">
         <b-tabs content-class="mt-3">
-            <b-tab title-link-class="dsff_tab" title="DSFF 2020" disabled></b-tab>
+            <b-tab title="DSFF 2020" active>
 
-            <b-tab title="Timetable" active>
+                <p>The fourth edition of the Drim Short Film Festival, undergoes its first online edition. Being faced with the new world order, the daily alterations imposed by the pandemic, and hence the inability of man to adapt to all invisible barriers and dangers, we were forced to choose the online edition as the safest option, which can be followed by everyone from 13-15 November on our website. We do not consider ourselves as (anti)heroes who will defy the reality, we are not the only festival that will oppose the virus, but for us maintaining the continuity of the festival is however a small but very precious victory, that will only allow the festival to grow and to become an inevitability, a mark and a need, having the reality of Struga collided with the film magic every year. </p>
+
+                <p>It’s worth mentioning how we managed to plan and direct another festival, that will gather the best short films from the past year in one place, carrying messages of life stories that represent struggles worth telling, from all around the world. At a time when distancing ourselves from one another means security and a way to fight the world pandemic, and physical closeness is merely a danger, art is obliged to connect the world on a higher level than the physical one, therefore creating a field of values that will freely ennoble people and try to fulfill their void. The art as a tremendous field, of course includes the film, as a medium through which we modulate our messages, wrapping them in stories, wrapping them  in newly created virtual worlds that will, for the umpteenth time, teach us to be human once again. As every year, our main focus is the short film form, providing it with the space it deserves, because it is direct and strong, and because it allows the film artists to appear on the big screen more often. </p>
+
+                <p>The time we live in shall not force us to easily reconcile with the new reality. The time we live in is however a consequence of the general world order in which we are being deeply involved. Once the virus will be overcome, it will be of great importance, whether we learn the lesson or simply throw the pandemic into the past, like every other forgotten crisis. Every film inevitably starts with the past, therefore whatever we manage to recognize in the characters, each of their action, each of their ventures, every success or defeat is indeed a consequence of their past. But by the end of the film, every well-built character grows, matures, changes, faces the past, so that he can step into the future. The past as well as the film contains a beginning, a culmination and an end.  And it is the confrontation with the past generally as a concept, both in the reality and in film, that is being the focus-theme of this year's edition of Drim Short. Hence the motto of the festival "The past has to end" comes into being. </p>
+
+
+            </b-tab>
+
+            <b-tab title="PROGRAM AND TIMETABLE 2020">
                 <div id="program_wrapper">
-                    <h4 class="program_date">Friday 13.11</h4>
-                    <p class="program_time">5pm – 10pm</p>
 
-                    <p class="program_p">International competition program</p>
-                    <ul>
-                        <li>“The distance between us and the sky” – Vasilis Kekatos 9’ | GRE | 2019</li>
-                        <li>"Exam"-Sonia K. Hadad 15’ | IR| 2019</li>
-                        <li>“The visit” - Djordje Vojvodic 20’ | MNE | 2020</li>
-                        <li>“Home is where the house is” – Peter Cerovsek 23’ | SLO/SER | 2019</li>
-                    </ul>
+                    <h4 class="program_date">
+                        DAY ONE / 13.11.20 / FRIDAY
+                    </h4>
 
-                    <p class="program_p">National competition program</p>
-                    <ul>
-                        <li>“I am calling your father” – Leon Ristov 12’ | MKD | 2020</li>
-                        <li>"Labour day waltz"- Zoran Stojkovski 17’ | CRO| 2019</li>
-                        <li>“Ficus” – Andrej Volkashin 17’ | BH, MKD | 2018</li>
-                        <li>“Ashes” – Gjorgji Lazov 18’| MKD | 2019</li>
-                    </ul>
+                    <p class="program_time">
+                        5PM - 10PM
+                    </p>
 
-                    <p class="program_p">Student films</p>
-                    <ul>
-                        <li>“Nobody told me I have to love you” – Matjaz Jamnik 18’ | SLO | 2019</li>
-                        <li>“Development” – Nikola Vucinic 22’ | MNE | 2020</li>
-                        <li>“Porodicni odmor” – Katarina Koljevic 19’ | SER | 2020</li>
-                        <li>"Dealing with the past’ competition program</li>
-                        <li>“B4” – Alen Simic 13’ | BiH| 2020</li>
-                        <li>“Foreign body” – Dusan Zoric 21’ | SER| 2018</li>
-                        <li>“Mother Mariam” – Mustafa Gurbuz 9’ | TUR | 2018</li>
-                        <li>“Unripe cherries” – Nebojsa Jovanovikj 16’ | MK | 2015</li>
-                        <li>Discussion "Mirror or dream’ Film as a tool for facing the past and imagining the future - 7pm</li>
-                    </ul>
-
-
-                    <h4 class="program_date">Saturday 14.11</h4>
-                    <p class="program_time">2pm</p>
+                    <p class="program_p">
+                        International competition program
+                    </p>
 
                     <ul>
-                        <li>Masterclass with Tamara Kotevska "Basics of making a documentary"</li>
+                        <li>"The distance between us and the sky" - Vasilis Kekatos 9' /GRE 2019</li>
+                        <li>"Exam"-Sonia K. Hadad 15' /IRL 2019</li>
+                        <li>"The visit" - Djordje Vojvodic 20' /MNE 2020 "Home is where the house is" - Peter Cerovsek 23' /SLO/SER 2019</li>
                     </ul>
 
-                    <p class="program_p">International competition program 5pm- 10 pm</p>
-                    <ul>
-                        <li>“The rudeness of a German lady” – Silva Capin 15’ | CRO/HUN | 2019</li>
-                        <li>“Massacre” – Maite Sonnet 26’ | FRA | 2020</li>
-                        <li>“Index” – Nicolas Kolovos 11’ | SWE/GRE | 2019</li>
-                    </ul>
-
-                    <p class="program_p">National competition program</p>
-                    <ul>
-                        <li>“Index 669” – Dejan Dzolev 16’ | MKD | 2018</li>
-                        <li>“Twisted” – Batuhan Ibrahim 18’ | MKD | 2020</li>
-                        <li>“Comma” – Sezer Salihi 10’ | MKD | 2019</li>
-                    </ul>
-
-                    <p class="program_p">Student competition program</p>
-                    <ul>
-                        <li>“Summer fruits” – Josip Lukic 20’ | CRO | 2019</li>
-                        <li>“Winter solstice” – Aleksa Mihajlovic 20’ | SER | 2020</li>
-                        <li>“Sherbet” – Nikola Stojanovic 19’ | SER | 2019</li>
-                        <li>"Dealing with the past’ competition program</li>
-                        <li>“Letters” – Bojan Radanovic 12’ | CRO | 2019</li>
-                        <li>“Faceless” – David Lusicic 14’ | CRO | 2020</li>
-                        <li>“Stack of material” – Sajra Subasic 7’ | BiH | 2019</li>
-                        <li>“Yet another departure” – Renata Poljak 11’ | CRO | 2019</li>
-                    </ul>
-
-                    <p class="program_p">Festival darlings</p>
-                    <ul>
-                        <li>“He loves my eyes” - Enxhi Rista 23’ | ROM | 2020</li>
-                        <li>“Aygol” - Dimitris Argyriou 8’ | RUS, GER| 2020</li>
-                        <li>“Sign” – Ugur Sahin | TUR | 2018</li>
-                        <li>“The kestrels” - Szonja SzabÓ | HU | 2020</li>
-                        <li>“Edgar” – Matko Abramic & Michael Reed 19’ | IRL/CRO | 2019</li>
-                    </ul>
-
-                    <h4 class="program_date">Sunday 15.11</h4>
-                    <p class="program_time">12pm - 5 pm</p>
+                    <p class="program_p">
+                        National competition program
+                    </p>
 
                     <ul>
-                        <li>Children’s program curated by Heinz Hermanns, in collaboration with KuKi Festival</li>
-                        <li>“Friends” – Florian Grolig 8’ |GER | 2019</li>
-                        <li>“Nest” – Sonja Rohleder 4’ | GER | 2019</li>
-                        <li>“Sloth” – Julia Ocker 4’ | GER | 2018</li>
-                        <li>“Cat lake city” – Antje Heyn 6’ | GER | 2019</li>
-                        <li>“Die lupe” – Nazgol Emami 10’ | GER | 2017</li>
-                        <li>“Voyagers” – Gauthier Ammeux 7’ | GER | 2017</li>
-                        <li>“The Kite” – Martin Smatana 13’ | GER | 2019</li>
-                        <li>"Ties"- Dina Velikovskaya 7’ | GER/RUS | 2019</li>
+                        <li>"I am calling your father" - Leon Ristov 12' /MKD 2020</li>
+                        <li>"Labour day waltz"- Zoran Stojkovski 17' /CRO 2019</li>
+                        <li>"Ficus" - Andrej Volkashin 17' /BiH, MKD 2018 "Ashes" - Gjorgji Lazov 18' /MKD 2019</li>
                     </ul>
 
-                    <p class="program_time">5pm – 10 pm</p>
-                    <p class="program_p">International competition program</p>
-                    <ul>
-                        <li>“I will end up in jail” – Alexandre Dostie 23’ | CAN | 2019</li>
-                        <li>“Route 3” - Thanasis Neofostisos 13’ | GRE | 2019</li>
-                        <li>“Bits” – Aron Horvath 15’ | SLO | 2020</li>
-                    </ul>
-
-                    <p class="program_p">National competition program</p>
+                    <p class="program_p">
+                        Student films
+                    </p>
 
                     <ul>
-                        <li>"Decision" – Agim Abdula 12’ | MKD| 2018</li>
-                        <li>“Dream of the blood and soul” – Igor Aleksov 20’ | MKD | 2018</li>
-                        <li>"Deaflands"-Bruno Veljanovski 6’ | MKD | 2020</li>
-                        <li>“The new beginning” – Tanja Simonovska 18’ | MKD | 2019</li>
+                        <li>"Nobody told me I have to love you" - Matjaz Jamnik 18' /SLO 2019</li>
+                        <li>"Development" - Nikola Vucinic 22'/MNE 2020 "Porodicni odmor" - Katarina Koljevic 19' /SER 2020</li>
                     </ul>
 
-                    <p class="program_p">Student competition program</p>
+                    <p class="program_p">
+                        `Dealing with the past' competition program
+                    </p>
 
                     <ul>
-                        <li>“Sad girl weekend” - Dimitris Tsakaleas & Lida Vartzioti 15’ | GRE | 2019</li>
-                        <li>“Kanya” - Apoorva Satish 15’ | IND | 2019</li>
-                        <li>“10 seconds” – Evgenia Taneva 6’ | BGR | 2020</li>
-                        <li>"Dealing with the past’ competition program</li>
-                        <li>“Shok” - Jamie Donoughue 21’ | XXK/GBR | 2015</li>
-                        <li>“Without Kosovo” - Dea Gjinovci 22’ | GBR | 2017</li>
-                        <li>“Faliminderit” - Martin Nicolas Neuhold 13’ | LUX | 2019</li>
-                        <li>“Yugoslavia” - Ademir Gogic 9 | BH | 2015</li>
+                        <li>"B4" - Alen Simic 13' /BIH 2020</li>
+                        <li>"Foreign body" - Dusan Zoric 21' /SER  2018</li>
+                        <li>"Mother Mariam" - Mustafa Gurbuz 9' /TUR 2018</li>
+                        <li>"Unripe cherries" - Nebojsa Jovanovikj 16' /MKD 2015</li>
                     </ul>
 
-                    <p class="program_p">Festival darlings</p>
+                    <p class="program_time">
+                        7PM
+                    </p>
+                    <ul>
+                        <li>
+                            Discussion 'Mirror or dream' Film as a tool for facing the past and imagining the future
+                        </li>
+                    </ul>
+
+                    <h4 class="program_date">
+                        DAY TWO / 14.11.20 / SATURDAY
+                    </h4>
+
+                    <p class="program_time">
+                        2PM
+                    </p>
+                    <ul>
+                        <li>
+                            Masterclass with Tamara Kotevska 'Basics of making a documentary'
+                        </li>
+                    </ul>
+
+                    <p class="program_time">
+                        5PM - 10PM
+                    </p>
+
+                    <p class="program_p">
+                        International competition program
+                    </p>
+
+                    <ul>
+                        <li>"The rudeness of a German lady" - Silva Capin 15' /CRO/HUN 2019</li>
+                        <li>"Massacre" - Maite Sonnet 26' /FRA 2020</li>
+                        <li>“Index" - Nicolas Kolovos 11' /SWE/GRE 2019</li>
+                    </ul>
+
+                    <p class="program_p">
+                        National competition program
+                    </p>
 
                     <ul>
                         <li>
-                            “TM” - Martin Ivanov/Ivica Dimitrijevikj 58’ | MKD | 2020
+                            "Index 669" - Dejan Djolev 16' /MKD 2018 "Twisted" - Batuhan Ibrahim 18' /MKD 2020
+                        </li>
+                        <li>
+                            "Comma" - Sezer Salihi 10' /MKD 2019
+                        </li>
+                    </ul>
+
+                    <p class="program_p">
+                        Student competition program
+                    </p>
+
+                    <ul>
+                        <li>"Summer fruits" - Josip Lukic 20' /CR0 2019 "Winter solstice" - Aleksa Mihajlovic 20' /SER 2020</li>
+                        <li>“Sherbet" - Nikola Stojanovic 19' /SER 2019</li>
+                    </ul>
+
+                    <p class="program_p">
+                        `Dealing with the past` competition program
+                    </p>
+
+                    <ul>
+                        <li>"Letters" - Bojan Radanovic 12' /CRO 2019 "Faceless" - David Lusicic 14' /CRO 2020</li>
+                        <li>"Stack of material" - Sajra Subasic 7' /BIH 2019</li>
+                        <li>"Yet another departure" - Renata Poljak 11' /CRO 2019</li>
+                    </ul>
+
+                    <p class="program_p">
+                        Festival darlings
+                    </p>
+
+                    <ul>
+                        <li>"He loves my eyes" - Enxhi Rista 23' /ROM 2020</li>
+                        <li>“Aygol" - Dimitris Argyriou 8' /RUS/GER 2020 "Sign" - Ugur Sahin 7’ /TUR 2018</li>
+                        <li>“The kestrels" - Szonja Szabo 17’ / HUN 2020</li>
+                        <li>"Edgar" - Matko Abramic & Michael Reed 19' /IRL/CR0 2019</li>
+                        <li>"Decision" - Agim Abdula 12' /MKD 2018</li>
+                    </ul>
+
+
+                    <h4 class="program_date">
+                        DAY THREE / 15.11.20 / SUNDAY
+                    </h4>
+
+                    <p class="program_time">
+                        12PM
+                    </p>
+
+                    <p class="program_p">
+                        Children's program curated by Heinz Hermanns, in collaboration with KuKi Festival
+                    </p>
+
+                    <ul>
+                        <li>"Friends" - Florian Grolig 8' /GER 2019</li>
+                        <li>"Nest" - Sonja Rohleder 4' /GER 2019</li>
+                        <li>"Sloth" - Julia Ocker 4' /GER 2018</li>
+                        <li>"Cat lake city" - Antje Heyn 6' /GER 2019</li>
+                        <li>"Die lupe" - Nazgol Emami 10' /GER 2017</li>
+                        <li>"Voyagers" - Gauthier Ammeux 7' /GER 2017</li>
+                        <li>"The Kite" - Martin Smatana 13' /GER 2019</li>
+                        <li>"Ties"- Dina Velikovskaya 7' /GER/RUS 2019</li>
+                    </ul>
+
+                    <p class="program_time">
+                        5PM - 10PM
+                    </p>
+
+                    <p class="program_p">
+                        International competition program
+                    </p>
+
+                    <ul>
+                        <li>"I will end up in jail" - Alexandre Dostie 23' /CAN 2019</li>
+                        <li>"Route 3" - Thanasis Neofostisos 13' /GRE 2019 "Bits" - Aron Horvath 15' / SLO 2020</li>
+                    </ul>
+
+                    <p class="program_p">
+                        National competition program
+                    </p>
+
+                    <ul>
+                        <li>"Dream of the blood and soul" - Igor Aleksov 20' /MKD 2018</li>
+                        <li>“Deaflands"- Bruno Veljanovski 6' /MKD 2020 "The new beginning" - Tanja Simonovska 18' /MKD 2019</li>
+                        <li>"Farmer's blues" - Iber Dheari 16' /MKD 2020</li>
+                    </ul>
+
+                    <p class="program_p">
+                        Student competition program
+                    </p>
+
+                    <ul>
+                        <li>"Kanya" - Apoorva Satish 15' /IND 2019</li>
+                        <li>"10 seconds" - Evgenia Taneva 6' /BGR 2020</li>
+                    </ul>
+
+
+                    <p class="program_p">
+                        `Dealing with the past` competition program
+                    </p>
+
+                    <ul>
+                        <li>"Shok" - Jamie Donoughue 21' /XXK/GBR 2015</li>
+                        <li>"Without Kosovo" Dea Gjinovci 22' /GBR 2017</li>
+                        <li>"Faliminderit" - Martin Nicolas Neuhold 13' /LUX 2019</li>
+                        <li>"Yugoslavia" - Ademir Gogic 9 /BIH 2015</li>
+                    </ul>
+
+                    <p class="program_p">
+                        Festival darlings
+                    </p>
+
+                    <ul>
+                        <li>
+                            "TM" - Martin Ivanov/Ivica Dimitrijevikj 58' /MKD 2020
                         </li>
                     </ul>
                 </div>
@@ -142,15 +230,7 @@
 
 
             <b-tab title="Catalogue">
-                <p>
-                Данлоад????
-                </p>
-                <p>
-                Дан?
-                </p>
-                <p>
-                Лоад?
-                </p>
+                <p>soon..</p>
             </b-tab>
         </b-tabs>
     </div>
