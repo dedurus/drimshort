f20a3818f0afdac30065f82d915968fa5d83e7e0
dedurus <dedurac@gmail.com>
2020-11-11 16:11:57 +0100

    removed icons loaded

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 
 import DefaultLayout from '~/layouts/Default.vue'
 
-import { BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+import BootstrapVue from 'bootstrap-vue'
 import './assets/styles/main.scss'
 import '~/assets/css/styles.css'
 
@@ -9,7 +9,6 @@ import checkIfMobile from './mixins/checkIfMobile'
 
 export default function (Vue, { router, head, isClient }) {
   Vue.use(BootstrapVue)
-  Vue.use(IconsPlugin)
   Vue.component('Layout', DefaultLayout)
   Vue.mixin(checkIfMobile)
 }
