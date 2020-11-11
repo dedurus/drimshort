01895d887b647678f6221c77dc6cc5d4d10e6f6c
dedurus <dedurac@gmail.com>
2020-11-11 16:11:39 +0100

    removed blog plugin

--- gridsome.config.js
+++ gridsome.config.js
@@ -2,21 +2,21 @@ module.exports = {
   siteName: 'Drim Short Film Festival',
   siteDescription: '',
   siteUrl: 'http://drimshortfestival.mk',
-  pathPrefix: '/site',
+  //pathPrefix: '/site',
   icon: './src/favicon.jpg',
   plugins: [
-    {
-      use: '@gridsome/source-filesystem',
-      options: {
-        path: 'blog/**/*.md',
-        typeName: 'BlogPost',
-        resolveAbsolutePaths: true,
-        remark: {
-          externalLinksTarget: '_blank',
-          externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-        },
-      },
-    },
+    // {
+    //   use: '@gridsome/source-filesystem',
+    //   options: {
+    //     path: 'blog/**/*.md',
+    //     typeName: 'BlogPost',
+    //     resolveAbsolutePaths: true,
+    //     remark: {
+    //       externalLinksTarget: '_blank',
+    //       externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+    //     },
+    //   },
+    // },
 
     /* {
       use: '@gridsome/plugin-google-analytics',
@@ -31,12 +31,12 @@ module.exports = {
       }
     } */
   ],
-  css: {
+  /* css: {
     loaderOptions: {
       scss: {
       }
     }
-  },
+  }, */
   devServer: {
     host: '0.0.0.0',
     port: 8080
